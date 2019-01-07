4d9c71a6ffbcc5cfdadf4746b197f3e6e6c6e204
Gurovij <georgiy.smirnov.2015@mail.ru>
2019-01-07 17:10:11 +0300

    Это первый наш коммит

---
+++ script.js
@@ -0,0 +1,16 @@
+let persone = {
+    name : "John",
+    age : 25
+};
+alert(persone.name);
+alert(persone["age"]);
+
+let arr = [
+    'pum.png',
+    'oran.png',
+    'apple.bmp'
+];
+
+console.log(arr[0]);
+confirm("Are you here?");
+prompt("GGGGG");
